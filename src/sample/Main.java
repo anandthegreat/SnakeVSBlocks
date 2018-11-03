85c0e363493e76dfe60e0b4bbb2b7353273f861a
Atul Anand <41585482+atul2938@users.noreply.github.com>
2018-11-03 11:23:35 +0530

    Changed fonts and colors

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -37,8 +37,6 @@ public class Main extends Application {
 
     ///////////////////////////////////////////////////////////////////////////////////////////
     @Override
-
-
     public void start(Stage primaryStage) throws Exception{
 
         Pane group = new Pane();                                                      //Changed root to group
@@ -54,16 +52,15 @@ public class Main extends Application {
         text.setX(65);
         text.setY(250);
         text.setFont(Font.font("verdana", FontWeight.BOLD, FontPosture.REGULAR, 75));
-        text.setFill(Color.BROWN);
+        text.setFill(Color.SEAGREEN);
 
         Button btn=new Button("Press ENTER");
         btn.setLayoutX(85);
         btn.setLayoutY(490);
         btn.setMinSize(200, 50);
-        btn.setStyle("-fx-font: 25 arial; -fx-base: #ee2211;");
+        btn.setStyle("-fx-font: 25 arial; -fx-base: #008080;");
 
         // action event
-
         EventHandler<ActionEvent> event = new EventHandler<ActionEvent>() {
 
             public void handle(ActionEvent e)
@@ -74,9 +71,10 @@ public class Main extends Application {
         };
 
         // when button is pressed
-
         btn.setOnAction(event);
-
+        
+        
+        // Blinking Effect in Button
         FadeTransition fadeTransition = new FadeTransition(Duration.seconds(4), btn);
         fadeTransition.setFromValue(1.0);
         fadeTransition.setToValue(0.6);
@@ -86,40 +84,45 @@ public class Main extends Application {
         group.getChildren().addAll(imageview,text,btn);
 
         Scene scene = new Scene(group, 600,800);
+        
         primaryStage.setTitle("SnakeVsBlock");
         primaryStage.setScene(scene);
         primaryStage.setResizable(false);
         primaryStage.show();
 
-
-
     }
-//////////////////////////////////////////////////////////////////////////////////////////
+    
+    //////////////////////////////////////////////////////////////////////////////////////////
+    
     public static class Blocks extends Rectangle {
-    int blockValue;                     //number written on the block
-    Blocks(int x, int y, int w, int h, int blockValue, Color color) {
+        private int blockValue;                     		//number written on the block
+        
+        Blocks(int x, int y, int w, int h, int blockValue, Color color) {
 
-        super(w, h, color);
-        this.blockValue = blockValue;
-        setTranslateX(x);
-        setTranslateY(y);
+            super(w, h, color);
+            this.blockValue = blockValue;
+            setTranslateX(x);
+            setTranslateY(y);
+
+        }
+        
+        void moveLeft() {
+            setTranslateX(getTranslateX() - 5);
+        }
+        
+        void moveRight() {
+
+            setTranslateX(getTranslateX() + 5);
+        }
+        
+        void moveUp() {
+            setTranslateY(getTranslateY() - 5);
+        }
+        void moveDown() {
+            setTranslateY(getTranslateY() + 5);
+        }
 
     }
-    void moveLeft() {
-        setTranslateX(getTranslateX() - 5);
-    }
-    void moveRight() {
-
-        setTranslateX(getTranslateX() + 5);
-    }
-    void moveUp() {
-        setTranslateY(getTranslateY() - 5);
-    }
-    void moveDown() {
-        setTranslateY(getTranslateY() + 5);
-    }
-
-}
     ///////////////////////////////////////////////////////////////////////////////////
 
     private void Menu(Stage primaryStage, ImageView imageview) {
@@ -131,7 +134,7 @@ public class Main extends Application {
         text.setX(65);
         text.setY(250);
         text.setFont(Font.font("verdana", FontWeight.BOLD, FontPosture.REGULAR, 75));
-        text.setFill(Color.BROWN);
+        text.setFill(Color.SEAGREEN);
 
 
 
@@ -139,7 +142,7 @@ public class Main extends Application {
         btn1.setLayoutX(95);
         btn1.setLayoutY(450);
         btn1.setMinSize(160, 40);
-        btn1.setStyle("-fx-font: 20 arial; -fx-base: #ffa500;");
+        btn1.setStyle("-fx-font: 20 arial; -fx-base: #008080;");
 
         EventHandler<ActionEvent> event1 = new EventHandler<ActionEvent>() {
             public void handle(ActionEvent e)
@@ -148,6 +151,8 @@ public class Main extends Application {
 
             }
         };
+        
+        // when button is pressed
         btn1.setOnAction(event1);
 
 
@@ -156,7 +161,7 @@ public class Main extends Application {
         btn2.setLayoutX(95);
         btn2.setLayoutY(510);
         btn2.setMinSize(160, 40);
-        btn2.setStyle("-fx-font: 20 arial; -fx-base: #ffa500;");
+        btn2.setStyle("-fx-font: 20 arial; -fx-base: #008080;");
 
         EventHandler<ActionEvent> event2 = new EventHandler<ActionEvent>() {
             public void handle(ActionEvent e)
@@ -165,6 +170,7 @@ public class Main extends Application {
 
             }
         };
+        // when button is pressed
         btn2.setOnAction(event2);
 
 
@@ -172,7 +178,7 @@ public class Main extends Application {
         btn3.setLayoutX(95);
         btn3.setLayoutY(570);
         btn3.setMinSize(160, 40);
-        btn3.setStyle("-fx-font: 20 arial; -fx-base: #ffa500;");
+        btn3.setStyle("-fx-font: 20 arial; -fx-base: #008080;");
 
         EventHandler<ActionEvent> event3 = new EventHandler<ActionEvent>() {
             public void handle(ActionEvent e)
@@ -181,16 +187,15 @@ public class Main extends Application {
 
             }
         };
+        
+        // when button is pressed
         btn3.setOnAction(event3);
-
-
-
 
         Button btn4=new Button("How to Play");
         btn4.setLayoutX(95);
         btn4.setLayoutY(630);
         btn4.setMinSize(160, 40);
-        btn4.setStyle("-fx-font: 20 arial; -fx-base: #ffa500;");
+        btn4.setStyle("-fx-font: 20 arial; -fx-base: #008080;");
 
 
         EventHandler<ActionEvent> event4 = new EventHandler<ActionEvent>() {
@@ -200,14 +205,15 @@ public class Main extends Application {
 
             }
         };
+        
+        // when button is pressed
         btn4.setOnAction(event4);
-
 
         Button btn5=new Button("Exit");
         btn5.setLayoutX(95);
         btn5.setLayoutY(690);
         btn5.setMinSize(160, 40);
-        btn5.setStyle("-fx-font: 20 arial; -fx-base: #ffa500;");
+        btn5.setStyle("-fx-font: 20 arial; -fx-base: #008080;");
 
         EventHandler<ActionEvent> event5 = new EventHandler<ActionEvent>() {
             public void handle(ActionEvent e)
@@ -218,18 +224,16 @@ public class Main extends Application {
             }
 
         };
+        
+        // when button is pressed
         btn5.setOnAction(event5);
 
         subroot.getChildren().addAll(imageview,text,btn1,btn2,btn3,btn4,btn5);
+        
         Scene scene=new Scene(subroot,600,800);
-
-//    	primaryStage.setTitle("SnakeVsBlock");
 
         primaryStage.setScene(scene);
 
-//        primaryStage.setResizable(false);
-
-//        primaryStage.show();
     }
 
 /////////////////////////////////////////////////////////////////////////////////////////
@@ -240,15 +244,13 @@ public class Main extends Application {
         AnimationTimer timer = new AnimationTimer() {
             @Override
             public void handle(long now) {
-               //update
+                //update
             }
         };
 
         timer.start();
 
-
         Pane play=new Pane();
-
 
         Image image = new Image("file:blackBackground.png");
         ImageView blackBackground=new ImageView(image);
@@ -263,7 +265,7 @@ public class Main extends Application {
         btn6.setLayoutX(220);
         btn6.setLayoutY(730);
         btn6.setMinSize(160, 40);
-        btn6.setStyle("-fx-font: 20 arial; -fx-base: #ffa500;");
+        btn6.setStyle("-fx-font: 20 arial; -fx-base: #87CEFA;");
 
         EventHandler<ActionEvent> event6 = new EventHandler<ActionEvent>() {
 
@@ -296,9 +298,6 @@ public class Main extends Application {
 
         primaryStage.setScene(scene);
 
-
-
-
     }
 
 
@@ -323,14 +322,14 @@ public class Main extends Application {
                 + "				Tokens : \n "
                 + "There are 4 token which could help you in scoring and \n "
                 + "tackling blocks. These are listed below: \n"
-                + "1.  \n"
-                + "2.  \n"
-                + "3.  \n"
-                + "4.  \n");
+                + "1.Ball : Increases snake's length by its value.  \n"
+                + "2.Destroy Blocks  : Destroy all blocks on screen and and \n 				 increse score by its value. \n"
+                + "3.Magnet : Attract coins towards the snake. \n"
+                + "4.Shield : Protects snake against any block for 5 seconds. \n");
 
         text.setX(70);
         text.setY(70);
-        text.setFont(Font.font("verdana", FontWeight.BOLD, FontPosture.REGULAR,15));
+        text.setFont(Font.font("Helvetica", FontWeight.BOLD, FontPosture.REGULAR,18));	   //changed Font to Helvetica
         text.setFill(Color.BROWN);
 
 
@@ -338,7 +337,7 @@ public class Main extends Application {
         btn6.setLayoutX(220);
         btn6.setLayoutY(650);
         btn6.setMinSize(160, 40);
-        btn6.setStyle("-fx-font: 20 arial; -fx-base: #ffa500;");
+        btn6.setStyle("-fx-font: 20 arial; -fx-base: #87CEFA;");
 
         EventHandler<ActionEvent> event6 = new EventHandler<ActionEvent>() {
 
@@ -350,7 +349,6 @@ public class Main extends Application {
         };
 
         // when button is pressed
-
         btn6.setOnAction(event6);
 
 
@@ -360,7 +358,7 @@ public class Main extends Application {
         r.setWidth(500);
         r.setHeight(700);
         r.setFill(Color.BEIGE);
-        r.setOpacity(10);
+        r.setOpacity(0.95);
 
         instructions.getChildren().setAll(imageview,r,text,btn6);
 
@@ -380,13 +378,17 @@ public class Main extends Application {
         ImageView blackBackground=new ImageView(image);
         blackBackground.setFitHeight(820);
         blackBackground.setFitWidth(620);
-
+        
+        Image temp = new Image("file:coming-soon.jpg");								// added coming-soon image
+        ImageView comingSoon=new ImageView(temp);
+        comingSoon.setFitHeight(820);
+        comingSoon.setFitWidth(620);
 
         Button btn6=new Button("Back to Menu");
         btn6.setLayoutX(220);
         btn6.setLayoutY(730);
         btn6.setMinSize(160, 40);
-        btn6.setStyle("-fx-font: 20 arial; -fx-base: #ffa500;");
+        btn6.setStyle("-fx-font: 20 arial; -fx-base: #87CEFA;");
 
         EventHandler<ActionEvent> event6 = new EventHandler<ActionEvent>() {
 
@@ -396,10 +398,11 @@ public class Main extends Application {
 
             }
         };
+        
         // when button is pressed
         btn6.setOnAction(event6);
 
-        leaderboard.getChildren().setAll(blackBackground,btn6);
+        leaderboard.getChildren().setAll(blackBackground,comingSoon,btn6);
 
         Scene scene=new Scene(leaderboard,600,800);
 
@@ -418,12 +421,17 @@ public class Main extends Application {
         ImageView blackBackground=new ImageView(image);
         blackBackground.setFitHeight(820);
         blackBackground.setFitWidth(620);
+        
+        Image temp = new Image("file:coming-soon.jpg");									// added coming-soon image
+        ImageView comingSoon=new ImageView(temp);
+        comingSoon.setFitHeight(820);
+        comingSoon.setFitWidth(620);
 
         Button btn6=new Button("Back to Menu");
         btn6.setLayoutX(220);
         btn6.setLayoutY(730);
         btn6.setMinSize(160, 40);
-        btn6.setStyle("-fx-font: 20 arial; -fx-base: #ffa500;");
+        btn6.setStyle("-fx-font: 20 arial; -fx-base: #87CEFA;");
 
         EventHandler<ActionEvent> event6 = new EventHandler<ActionEvent>() {
 
@@ -436,7 +444,7 @@ public class Main extends Application {
         // when button is pressed
         btn6.setOnAction(event6);
 
-        themes.getChildren().setAll(blackBackground,btn6);
+        themes.getChildren().setAll(blackBackground,comingSoon,btn6);
 
         Scene scene=new Scene(themes,600,800);
 
@@ -445,18 +453,8 @@ public class Main extends Application {
 
     }
 
-
-
     public static void main(String[] args) {
 
         launch(args);
-
-
-
     }
-
-
-
-
-
 }
