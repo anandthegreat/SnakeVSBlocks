d432e323091695ed1e8e226086f8d37481ea1e68
Atul Anand <41585482+atul2938@users.noreply.github.com>
2018-11-08 10:41:58 +0530

    Added left and right. Use this Block Class.

---
+++ src/sample/Block.java
@@ -0,0 +1,36 @@
+package sample;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+
+public class Block extends Rectangle {
+    private int blockValue;                     		//number written on the block
+
+    public Block(int x, int y, int w, int h, int blockValue, Color color) {
+
+        super(w, h, color);
+        super.setArcHeight(25);
+        super.setArcWidth(25);
+        this.blockValue = blockValue;
+        setTranslateX(x);
+        setTranslateY(y);
+//        this.setArcHeight(25);
+//        this.setArcWidth(25);
+
+    }
+    public void setblockValue(int blockValue) {
+        this.blockValue=blockValue;
+    }
+    void moveLeft() {
+        setTranslateX(getTranslateX() - 5);
+    }
+    void moveRight() { 
+    	setTranslateX(getTranslateX() + 5); 
+    }
+    void moveUp() {
+        setTranslateY(getTranslateY() - 5);
+    }
+    void moveDown() {
+        setTranslateY(getTranslateY() + 5);
+    }
+
+}
