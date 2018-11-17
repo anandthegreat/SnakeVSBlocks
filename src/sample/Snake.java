28e49ad00d320f754da0dbdbee8b56c82c406819
Anand <anand17218@iiitd.ac.in>
2018-11-17 13:17:57 +0530

    Smooth Snake commented,has some issues

--- src/sample/Snake.java
+++ src/sample/Snake.java
@@ -13,7 +13,7 @@ public class Snake {
     private int numBalls;
     protected Text T;
     List<Circle> body;
-    public static double snakePos=270;
+  //  public static double snakePos=270;
 
     Snake(){
         
@@ -35,7 +35,7 @@ public class Snake {
     
     public void setScoreText() {
     	if(numBalls>0 && this.body.size()>0) {
-    		double x=snakePos-5;
+            double x=this.body.get(0).getCenterX()-5;
         	double y=this.body.get(0).getCenterY()+2;
         	
         	T.setX(x);
@@ -49,55 +49,58 @@ public class Snake {
     	return T;
     }
 
-    class smoothSnake{
-        public void swiftSnake(TranslateTransition translate,int i,Snake snake,int direction){
-            if(i==0){
-                if(direction==0)
-                {translate.setByX(-20);
-                    Snake.snakePos=250+snake.body.get(0).getTranslateX();
-                }
-                else {
-                    Snake.snakePos=290+snake.body.get(0).getTranslateX();
-                    translate.setByX(+20);
-                }
-                translate.setDuration(Duration.millis(50));
-                translate.play();
-                snake.setScoreText();
-            }
-            else {
-                //System.out.println(snake.body.get(0).getTranslateX());
-                if(direction==0)
-                translate.setToX(snake.body.get(0).getTranslateX()-20);
-                else
-                    translate.setToX(snake.body.get(0).getTranslateX()+20);
-                translate.setDuration(Duration.millis(i*60));
-                translate.play();
-            }
-
-        }
-
-    }
+//    class smoothSnake{
+//        public void swiftSnake(TranslateTransition translate,int i,Snake snake,int direction){
+//            if(i==0){
+//                if(direction==0)
+//                {translate.setByX(-20);
+//                    Snake.snakePos=250+snake.body.get(0).getTranslateX();
+//                }
+//                else {
+//                    Snake.snakePos=290+snake.body.get(0).getTranslateX();
+//                    translate.setByX(+20);
+//                }
+//                translate.setDuration(Duration.millis(50));
+//                translate.play();
+//                snake.setScoreText();
+//            }
+//            else {
+//                //System.out.println(snake.body.get(0).getTranslateX());
+//                if(direction==0)
+//                translate.setToX(snake.body.get(0).getTranslateX()-20);
+//                else
+//                    translate.setToX(snake.body.get(0).getTranslateX()+20);
+//                translate.setDuration(Duration.millis(i*60));
+//                translate.play();
+//            }
+//
+//        }
+//
+//    }
 
     public void moveLeft() {
-        //this.body.get(0).setCenterX(this.body.get(0).getCenterX()-20);
+
     	for(int i=0;i<this.body.size();i++){
-            TranslateTransition translate = new TranslateTransition();
-            translate.setNode(this.body.get(i));
-            smoothSnake obj=new smoothSnake();
-            obj.swiftSnake(translate,i,this,0);        //0 for left, 1 for right
+//            TranslateTransition translate = new TranslateTransition();
+//            translate.setNode(this.body.get(i));
+//            smoothSnake obj=new smoothSnake();
+//            obj.swiftSnake(translate,i,this,0);        //0 for left, 1 for right
+              this.body.get(i).setCenterX(this.body.get(i).getCenterX()-20);
         }
     	setScoreText();
 
     }
     public void moveRight() {
-       // this.body.get(0).setCenterX(this.body.get(0).getCenterX()+20);
+
 
             for(int i=0;i<this.body.size();i++){
-                TranslateTransition translate = new TranslateTransition();
-                translate.setNode(this.body.get(i));
-                smoothSnake obj=new smoothSnake();
-                obj.swiftSnake(translate,i,this,1);
-            }
+//                TranslateTransition translate = new TranslateTransition();
+//                translate.setNode(this.body.get(i));
+//                smoothSnake obj=new smoothSnake();
+//                obj.swiftSnake(translate,i,this,1);
+
+                 this.body.get(i).setCenterX(this.body.get(i).getCenterX()+20);
+                }
 
     	setScoreText();
     }
